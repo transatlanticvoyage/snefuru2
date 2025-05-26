45ed31df3c2777a11017decf9c79167d4df82f31
transatlanticvoyage <sandbagexpert@gmail.com>
2025-05-26 14:20:54 -0400

    dsf

--- client/src/pages/image_handler/screen1.tsx
+++ client/src/pages/image_handler/screen1.tsx
@@ -174,41 +174,52 @@ const ImageHandlerScreen1: React.FC = () => {
           <table className="min-w-full divide-y divide-gray-200">
             <thead className="bg-gray-100">
               <tr>
-                <th className="p-2 w-10 bg-gray-200">global_id</th>
-                <th className="p-2 bg-yellow-100">rel_images2_batch_id</th>
-                <th className="p-2 bg-yellow-100">batch date created</th>
-                <th className="p-2 bg-yellow-100">batch name</th>
-                <th className="p-2 bg-gray-200">batch total images (dynamic pull field qty)</th>
-                <th className="p-2 bg-gray-200">image preview</th>
-                <th className="p-2 bg-gray-200">img_url1</th>
-                <th className="p-2 bg-gray-200">image extension</th>
-                <th className="p-2 bg-gray-200">img_file_size</th>
-                <th className="p-2 bg-gray-200">width5</th>
-                <th className="p-2 bg-gray-200">height5</th>
-                <th className="p-2 bg-gray-200">folder</th>
-                <th className="p-2 bg-pink-200">img_zpf_code</th>
-                <th className="p-2 bg-pink-200">width1</th>
-                <th className="p-2 bg-pink-200">height1</th>
-                <th className="p-2 bg-pink-200">associated_text_content_on_page1</th>
-                <th className="p-2 bg-pink-200">file_name1</th>
-                <th className="p-2 bg-pink-200">more_instructions1</th>
-                <th className="p-2 bg-pink-200">prompt1</th>
-                <th className="p-2 bg-pink-200">ai_tool1</th>
-                <th className="bg-black w-[5px]" />
-                <th className="p-2 bg-blue-100">meta_data_wiped1</th>
-                <th className="p-2 bg-blue-100">screenshotted1</th>
-                <th className="bg-black w-[5px]" />
+                <th className="p-2 border border-gray-700 w-10 bg-white align-middle">
+                  <Checkbox />
+                </th>
+                <th className="p-2 border border-gray-700 bg-white align-middle">Actions</th>
+                <th className="p-2 border border-gray-700 w-10 bg-gray-200 align-middle">global_id</th>
+                <th className="p-2 border border-gray-700 bg-yellow-100 align-middle">rel_images2_batch_id</th>
+                <th className="p-2 border border-gray-700 bg-yellow-100 align-middle">batch date created</th>
+                <th className="p-2 border border-gray-700 bg-yellow-100 align-middle">batch name</th>
+                <th className="p-2 border border-gray-700 bg-gray-200 align-middle">batch total images (dynamic pull field qty)</th>
+                <th className="p-2 border border-gray-700 bg-gray-200 align-middle">image preview</th>
+                <th className="p-2 border border-gray-700 bg-gray-200 align-middle">img_url1</th>
+                <th className="p-2 border border-gray-700 bg-gray-200 align-middle">image extension</th>
+                <th className="p-2 border border-gray-700 bg-gray-200 align-middle">img_file_size</th>
+                <th className="p-2 border border-gray-700 bg-gray-200 align-middle">width5</th>
+                <th className="p-2 border border-gray-700 bg-gray-200 align-middle">height5</th>
+                <th className="p-2 border border-gray-700 bg-gray-200 align-middle">folder</th>
+                <th className="p-2 border border-gray-700 bg-pink-200 align-middle">img_zpf_code</th>
+                <th className="p-2 border border-gray-700 bg-pink-200 align-middle">width1</th>
+                <th className="p-2 border border-gray-700 bg-pink-200 align-middle">height1</th>
+                <th className="p-2 border border-gray-700 bg-pink-200 align-middle">associated_text_content_on_page1</th>
+                <th className="p-2 border border-gray-700 bg-pink-200 align-middle">file_name1</th>
+                <th className="p-2 border border-gray-700 bg-pink-200 align-middle">more_instructions1</th>
+                <th className="p-2 border border-gray-700 bg-pink-200 align-middle">prompt1</th>
+                <th className="p-2 border border-gray-700 bg-pink-200 align-middle">ai_tool1</th>
+                <th className="bg-black w-[5px] border border-gray-700 align-middle" />
+                <th className="p-2 border border-gray-700 bg-blue-100 align-middle">meta_data_wiped1</th>
+                <th className="p-2 border border-gray-700 bg-blue-100 align-middle">screenshotted1</th>
+                <th className="bg-black w-[5px] border border-gray-700 align-middle" />
               </tr>
             </thead>
             <tbody className="divide-y divide-gray-200">
               {imageData.map((image) => (
                 <tr key={image.id} className="hover:bg-gray-50">
-                  <td className="p-2">{image.id}</td>
-                  <td className="p-2">{image.img_url2}</td>
-                  <td className="p-2">{image.img_url3}</td>
-                  <td className="p-2">{image.img_url4}</td>
-                  <td className="p-2">{image.img_url5}</td>
-                  <td className="p-2">
+                  <td className="p-2 border border-gray-700 align-middle">
+                    <Checkbox />
+                  </td>
+                  <td className="p-2 border border-gray-700 align-middle">
+                    <Button variant="outline" size="sm" className="mr-1">Edit</Button>
+                    <Button variant="outline" size="sm" className="text-red-500">Delete</Button>
+                  </td>
+                  <td className="p-2 border border-gray-700 align-middle">{image.id}</td>
+                  <td className="p-2 border border-gray-700 align-middle">{image.img_url2}</td>
+                  <td className="p-2 border border-gray-700 align-middle">{image.img_url3}</td>
+                  <td className="p-2 border border-gray-700 align-middle">{image.img_url4}</td>
+                  <td className="p-2 border border-gray-700 align-middle">{image.img_url5}</td>
+                  <td className="p-2 border border-gray-700 align-middle">
                     <div className="w-12 h-12 bg-gray-200 rounded overflow-hidden">
                       <img 
                         src={`https://picsum.photos/seed/${image.id}/100/100`} 
@@ -217,16 +228,12 @@ const ImageHandlerScreen1: React.FC = () => {
                       />
                     </div>
                   </td>
-                  <td className="p-2">{image.filename}</td>
-                  <td className="p-2">{image.filesize}</td>
-                  <td className="p-2 max-w-xs truncate">{image.path}</td>
-                  <td className="p-2">{image.img_url1}</td>
-                  <td className="p-2">{image.presented_1}</td>
-                  <td className="p-2 max-w-xs truncate">{image.nice_name_actual}</td>
-                  <td className="p-2">
-                    <Button variant="outline" size="sm" className="mr-1">Edit</Button>
-                    <Button variant="outline" size="sm" className="text-red-500">Delete</Button>
-                  </td>
+                  <td className="p-2 border border-gray-700 align-middle">{image.filename}</td>
+                  <td className="p-2 border border-gray-700 align-middle">{image.filesize}</td>
+                  <td className="p-2 border border-gray-700 align-middle max-w-xs truncate">{image.path}</td>
+                  <td className="p-2 border border-gray-700 align-middle">{image.img_url1}</td>
+                  <td className="p-2 border border-gray-700 align-middle">{image.presented_1}</td>
+                  <td className="p-2 border border-gray-700 align-middle max-w-xs truncate">{image.nice_name_actual}</td>
                 </tr>
               ))}
             </tbody>
